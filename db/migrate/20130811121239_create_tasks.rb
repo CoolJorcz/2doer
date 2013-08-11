f575fbcc9e61d224c8c03dc4a609167f9eecd1c5
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-11 13:49:04 -0500

    add target time, completed at time, and tweeted time to task migrations

---
+++ db/migrate/20130811121239_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :notes
+      t.string :status
+      t.integer :doer_id
+      t.integer :asker_id
+      t.datetime :goal_time
+      t.datetime :tweeted_at
+      t.datetime :completed_at
+
+      t.timestamps 
+    end
+  end
+end

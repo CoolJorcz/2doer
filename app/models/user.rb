059e754a96233b20de09a64938a5d7e727e4b0d1
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-11 15:32:03 -0500

    enabled user login through twitter with persistance

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ActiveRecord::Base
   has_many :obligations, :class_name => "Task"
   has_many :requests, :class_name => "Task"
 
-
+  def self.login(res)
+    @user         = User.where(:uid => res["uid"]).first_or_initialize
+    @user.handle  = res['info']['nickname']
+    @user.name    = res['info']['name']
+    @user.token   = res['credentials']['token']
+    @user.secret  = res['credentials']['secret']
+    @user.save
+    @user
+  end
   # def create_client
   #   Twitter::Client.new(
   #       :oauth_token => self.oauth_token,

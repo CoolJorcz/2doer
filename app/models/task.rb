c587ac8c07f7e8a074e2860338837aee642cbc85
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-11 13:05:06 -0500

    fix typo in belongs to

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-  belong_to :doer, :class_name => "User"
-  belong_to :asker, :class_name => "User"
+  belongs_to :doer, :class_name => "User"
+  belongs_to :asker, :class_name => "User"
 end

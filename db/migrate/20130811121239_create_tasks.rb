05859d9a8d6d8a8d7ca3aebb4b9b0c801100beb0
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-11 16:08:08 -0500

    user can send todo request

--- db/migrate/20130811121239_create_tasks.rb
+++ db/migrate/20130811121239_create_tasks.rb
@@ -1,11 +1,11 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :notes
-      t.string :status
-      t.integer :doer_id
-      t.integer :asker_id
+      t.string   :body
+      t.text     :notes
+      t.string   :status
+      t.integer  :doer_id
+      t.integer  :asker_id
       t.datetime :goal_time
       t.datetime :tweeted_at
       t.datetime :completed_at
